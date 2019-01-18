e7bc6fd88e9d27167f9691aa944b2894d9b456ff
Rob Marshall <rob@fox.agency>
2019-01-18 16:31:21 +0000

    Fixed beforeEach

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
+let wrapped;
+
 beforeEach(() => {
-    let wrapped;
+    wrapped = mount(<CommentBox />);
 });
 
 it('has a text area and button', () => {
-    wrapped = mount(<CommentBox />);
-
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
