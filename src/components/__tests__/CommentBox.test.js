ccda632f1f7dc7fe59d68788064601cd02158f0e
Rob Marshall <rob@fox.agency>
2019-01-18 16:30:41 +0000

    Added beforeEach

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
+beforeEach(() => {
+    let wrapped;
+});
+
 it('has a text area and button', () => {
-    const wrapped = mount(<CommentBox />);
-    
+    wrapped = mount(<CommentBox />);
+
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
