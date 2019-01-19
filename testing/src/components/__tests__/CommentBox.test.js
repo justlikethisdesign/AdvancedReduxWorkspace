caacc1d18c7cab2794a82438711607acf70c52ea
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-01-19 12:24:10 +0000

    Added comment box test

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,16 @@ it('has a text area and button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+
+    const comment = 'new comment';
+
+    wrapped.find('textarea').simulate('change', {
+        target: { value: comment }
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+
+});
