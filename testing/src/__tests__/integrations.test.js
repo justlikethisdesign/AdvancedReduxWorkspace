03a99d2049343e316d07e4cd643c099a8c5825d7
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-01-19 18:45:46 +0000

    Added test to check button click gets comments and adds lis

---
+++ testing/src/__tests__/integrations.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios'
+import Root from 'Root';
+import App from 'components/app';
+
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{ name: 'Fetched #1'}, { name: 'Fetched #2'}]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', () => {
+    // Attempt to render entire app
+    const wrapped = mount(
+        <Root>
+            <App />
+        </Root>
+    );
+
+    // Find 'fetchComments' button - click it
+    wrapped.find('.fetch-comments').simulate('click');
+
+    setTimeout(() => {
+        // Expect to find a list of comments - LIs
+        expect(wrapped.find('li').length).toEqual(2);
+    }, 100);
+
+});
