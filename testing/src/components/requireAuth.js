4239e8cf749b814a5e1e337a4fdddd13a3cec7de
Rob Marshall <rob@fox.agency>
2019-01-21 14:27:22 +0000

    Added HOC

---
+++ testing/src/components/requireAuth.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default ChildComponent => {
+
+    class ComposedComponent extends Component {
+
+        render() {
+            return <ChildComponent />;
+        }
+
+    }
+
+    return ComposedComponent;
+
+};
