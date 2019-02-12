320ffb2b6425980d920ca601389cc2a79a10681c
Rob Marshall <rob@fox.agency>
2019-02-12 17:50:39 +0000

    Added passport

--- testing/server/router.js
+++ testing/server/router.js
@@ -1,5 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// Passport automatically wants to create sessions -
+// we are using tokens so we dont want that
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app){
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    });
     app.post('/signup', Authentication.signup);
 }
