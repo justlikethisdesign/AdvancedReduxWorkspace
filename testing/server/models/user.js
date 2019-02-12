856da7464ffa95c32bf116da76132c6956a40e08
Rob Marshall <rob@fox.agency>
2019-02-12 16:13:22 +0000

    Added bcrypt to the user account password

--- testing/server/models/user.js
+++ testing/server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
     email: {
@@ -10,6 +11,26 @@ const userSchema = new Schema({
     password: {
         type: String
     }
+});
+
+// On Save Hook, excrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    // Generate a salt, then run callback
+    bcrypt.genSalt(10, function(err, salt){
+        if(err){ return next(err); }
+
+        // Hash password using the salt
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) { return next(err); }
+
+            // overwrite plain text password, with encrypted model
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const ModelClass = mongoose.model('user', userSchema);
