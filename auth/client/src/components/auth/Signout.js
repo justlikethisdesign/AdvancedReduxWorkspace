dc46a2b38d3eba29b1f895783d6cc5acc71bf2b0
Rob Marshall <rob@fox.agency>
2019-02-15 12:14:08 +0000

    Added functionality

--- auth/client/src/components/auth/Signout.js
+++ auth/client/src/components/auth/Signout.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Signout extends Component {
+
+    // When component appears on the screen - run the signout action function
+    componentDidMount() {
+        this.props.signout();
+    }
+
     render () {
         return <div>Bi for now</div>
     }
 }
 
-export default Signout;
+export default connect(null, actions)(Signout);
